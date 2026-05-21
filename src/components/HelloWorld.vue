b6e10f8e41f1e87bf9f8e537a839724045ed593d
Khunpro <gmv95553@gmail.com>
2026-05-21 14:48:36 +0700

    feat: New Free

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,7 +16,7 @@ const count = ref(0)
     </div>
     <div>
       <h1>maker</h1>
-      <p>Edit <code>src/App.vue</code> and save to test <code>HMR</code></p>
+      <p>free <code>src/App.vue</code> and save to test <code>HMR</code></p>
     </div>
     <button type="button" class="counter" @click="count++">
       Count is {{ count }}
